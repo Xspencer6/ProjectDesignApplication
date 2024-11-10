6168e775b73fe923c5a8fb780fad68291e95bb52
Spencer <spenceralmodiel@gmail.com>
2024-11-10 18:05:02 +0800

    Uploaded whole android application project to Github

---
+++ app/src/main/java/com/example/tilespekt/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.tilespekt
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.forEach
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Applying custom text appearance to BottomNavigationView menu items
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavView.menu.forEach { menuItem ->
+            val menuItemView = bottomNavView.findViewById<View>(menuItem.itemId)
+            menuItemView?.let {
+                val label1 = menuItemView.findViewById<TextView>(R.id.history)
+                val label2 = menuItemView.findViewById<TextView>(R.id.mapping)
+                val label3 = menuItemView.findViewById<TextView>(R.id.dashboard)
+                label1?.setTextAppearance(R.style.BottomNavTextAppearance)
+                label2?.setTextAppearance(R.style.BottomNavTextAppearance)
+                label3?.setTextAppearance(R.style.BottomNavTextAppearance)
+            }
+        }
+    }
+}
